2b54a74badca47057ce7eb9db7e9f6dac5ff72ec
FedeWade <federico.ballarin.2@studenti.unipd.it>
2021-04-19 19:11:30 +0200

    added alt

--- src/Components/SingleDayTable.js
+++ src/Components/SingleDayTable.js
@@ -6,7 +6,7 @@ import callLogo from "../callicon.png";
 class SingleDayTable extends React.Component {
   chiamata(h) {
     if (h === "12Chiamata" || h === "19Chiamata") {
-      return <img src={callLogo} width="47" height="47"></img>;
+      return <img src={callLogo} width="47" height="47" alt="Chiamata"></img>;
     }
     return h;
   }
