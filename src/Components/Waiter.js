b91409cd6b76867e169829cf665132f7877edc5a
FedeWade <federico.ballarin.2@studenti.unipd.it>
2021-04-19 23:56:11 +0200

    a

--- src/Components/Waiter.js
+++ src/Components/Waiter.js
@@ -10,15 +10,15 @@ class Waiter extends React.Component {
   }
 
   getBgColor(name) {
-    if (name === "Federico") return "#d2691e";
+    if (name === "Federico") return "#FF7F50";
     else if (name === "Valentina") return "#5b5ea6";
+    else if (name === "Stefano") return "#672e3b";
     else if (name === "Luca") return "#b93a32";
-    else if (name === "Paolo") return "#d5ae41";
-    else if (name === "Sergio") return "#672e3b";
-    else if (name === "Stefano") return "#79c753";
+    else if (name === "Paolo") return "#9932CC";
+    else if (name === "Sergio") return "#79c753";
     else if (name === "Michela") return "#92b6d5";
     else if (name === "Claudia") return "#c48f65";
-    else if (name === "Sara") return "#006e6d";
+    else if (name === "Sara") return "#20B2AA";
   }
 
   render() {
