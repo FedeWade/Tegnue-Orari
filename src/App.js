b37dbd545f636fa0abb9af076c5c7597953888d9
FedeWade <federico.ballarin.2@studenti.unipd.it>
2021-04-23 23:25:38 +0200

    added advancing in week feature

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import SingleDayTable from "./Components/SingleDayTable";
 import DayNavBar from "./Components/DayNavBar";
 import HeaderBar from "./Components/HeaderBar";
-
 import firebase from "firebase";
-
 import React from "react";
 
 class App extends React.Component {
@@ -20,37 +18,8 @@ class App extends React.Component {
       domenicaSprints: [],
       currentWeek: [],
     };
-  }
+    this.changeToNextWeek = this.changeToNextWeek.bind(this);
 
-  fetchDaySprintsFromDB(day) {
-    const lunediRef = firebase.database().ref(day);
-    lunediRef.on("value", (snapshot) => {
-      let sprints = snapshot.val();
-      let newState = [];
-      for (let sprint in sprints) {
-        newState.push({
-          hour: sprint,
-          waiters: sprints[sprint],
-        });
-      }
-      let stateName = day + "Sprints";
-      this.setState({
-        [stateName]: newState,
-      });
-    });
-  }
-
-  componentDidMount() {
-    this.fetchDaySprintsFromDB("lunedi");
-    this.fetchDaySprintsFromDB("martedi");
-    this.fetchDaySprintsFromDB("mercoledi");
-    this.fetchDaySprintsFromDB("giovedi");
-    this.fetchDaySprintsFromDB("venerdi");
-    this.fetchDaySprintsFromDB("sabato");
-    this.fetchDaySprintsFromDB("domenica");
-  }
-
-  render() {
     let currentDate = new Date();
     let currentDayOfWeek = currentDate.getDay();
     let currentDayOfMonth = currentDate.getDate();
@@ -84,51 +53,118 @@ class App extends React.Component {
     this.state.currentWeek.push(venerdi);
     this.state.currentWeek.push(sabato);
     this.state.currentWeek.push(domenica);
+  }
 
+  fetchDaySprintsFromDB(date, day) {
+    const lunediRef = firebase.database().ref(day + date);
+    lunediRef.on("value", (snapshot) => {
+      let sprints = snapshot.val();
+      let newState = [];
+      for (let sprint in sprints) {
+        newState.push({
+          hour: sprint,
+          waiters: sprints[sprint],
+        });
+      }
+      let stateName = day + "Sprints";
+      this.setState({
+        [stateName]: newState,
+      });
+    });
+  }
+
+  fetchCurrentWeekFromDB() {
+    this.fetchDaySprintsFromDB(this.state.currentWeek[0].getDate(), "lunedi");
+    this.fetchDaySprintsFromDB(this.state.currentWeek[1].getDate(), "martedi");
+    this.fetchDaySprintsFromDB(
+      this.state.currentWeek[2].getDate(),
+      "mercoledi"
+    );
+    this.fetchDaySprintsFromDB(this.state.currentWeek[3].getDate(), "giovedi");
+    this.fetchDaySprintsFromDB(this.state.currentWeek[4].getDate(), "venerdi");
+    this.fetchDaySprintsFromDB(this.state.currentWeek[5].getDate(), "sabato");
+    this.fetchDaySprintsFromDB(this.state.currentWeek[6].getDate(), "domenica");
+  }
+
+  componentDidMount() {
+    this.fetchCurrentWeekFromDB();
+  }
+
+  addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  changeToNextWeek() {
+    let newWeek = [];
+    newWeek.push(this.addDays(this.state.currentWeek[0], 7));
+    newWeek.push(this.addDays(this.state.currentWeek[1], 7));
+    newWeek.push(this.addDays(this.state.currentWeek[2], 7));
+    newWeek.push(this.addDays(this.state.currentWeek[3], 7));
+    newWeek.push(this.addDays(this.state.currentWeek[4], 7));
+    newWeek.push(this.addDays(this.state.currentWeek[5], 7));
+    newWeek.push(this.addDays(this.state.currentWeek[6], 7));
+
+    this.setState({ currentWeek: newWeek });
+
+    //this.fetchCurrentWeekFromDB();
+    this.fetchDaySprintsFromDB(newWeek[0].getDate(), "lunedi");
+    this.fetchDaySprintsFromDB(newWeek[1].getDate(), "martedi");
+    this.fetchDaySprintsFromDB(newWeek[2].getDate(), "mercoledi");
+    this.fetchDaySprintsFromDB(newWeek[3].getDate(), "giovedi");
+    this.fetchDaySprintsFromDB(newWeek[4].getDate(), "venerdi");
+    this.fetchDaySprintsFromDB(newWeek[5].getDate(), "sabato");
+    this.fetchDaySprintsFromDB(newWeek[6].getDate(), "domenica");
+  }
+
+  render() {
     return (
       <div className="app">
         <DayNavBar currentWeek={this.state.currentWeek}></DayNavBar>
         <HeaderBar></HeaderBar>
+        <button onClick={this.changeToNextWeek}>Prossima settimana</button>
+
         <div className="tablesWrapper">
           <SingleDayTable
             day="Lunedì"
-            date={lunedi.getDate()}
+            date={this.state.currentWeek[0].getDate()}
             sprints={this.state.lunediSprints}
           ></SingleDayTable>
 
           <SingleDayTable
             day="Martedì"
-            date={martedi.getDate()}
+            date={this.state.currentWeek[1].getDate()}
             sprints={this.state.martediSprints}
           ></SingleDayTable>
 
           <SingleDayTable
             day="Mercoledì"
-            date={mercoledi.getDate()}
+            date={this.state.currentWeek[2].getDate()}
             sprints={this.state.mercolediSprints}
           ></SingleDayTable>
 
           <SingleDayTable
             day="Giovedì"
-            date={giovedi.getDate()}
+            date={this.state.currentWeek[3].getDate()}
             sprints={this.state.giovediSprints}
           ></SingleDayTable>
 
           <SingleDayTable
             day="Venerdì"
-            date={venerdi.getDate()}
+            date={this.state.currentWeek[4].getDate()}
             sprints={this.state.venerdiSprints}
           ></SingleDayTable>
 
           <SingleDayTable
             day="Sabato"
-            date={sabato.getDate()}
+            date={this.state.currentWeek[5].getDate()}
             sprints={this.state.sabatoSprints}
           ></SingleDayTable>
 
           <SingleDayTable
             day="Domenica"
-            date={domenica.getDate()}
+            date={this.state.currentWeek[6].getDate()}
             sprints={this.state.domenicaSprints}
           ></SingleDayTable>
         </div>
