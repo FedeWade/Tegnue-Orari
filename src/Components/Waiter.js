e6a9c4b4ff421c6a2da0dc0ad67b25a0b6297231
FedeWade <federico.ballarin.2@studenti.unipd.it>
2021-06-11 23:11:45 +0200

    changed waiter color

--- src/Components/Waiter.js
+++ src/Components/Waiter.js
@@ -27,7 +27,7 @@ class Waiter extends React.Component {
     else if (name === "Cesare") return "#f2c40d";
     else if (name === "Raluca") return "#006e0f";
     else if (name === "Eleonora") return "#cb42f5";
-    else if (name === "Bibbò") return "#c1502e";
+    else if (name === "Bibbò") return "#004080";
   }
 
   render() {
