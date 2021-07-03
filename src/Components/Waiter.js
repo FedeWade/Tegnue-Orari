6f80969db3af96b071985833183c041bc9b120a8
FedeWade <federico.ballarin.2@studenti.unipd.it>
2021-07-04 01:04:24 +0200

    added modu name

--- src/Components/Waiter.js
+++ src/Components/Waiter.js
@@ -23,7 +23,7 @@ class Waiter extends React.Component {
     else if (name === "Federico") return "#79c753";
     else if (name === "Jacopo") return "#75b7f0";
     else if (name === "Giulia" || name === "Girotti") return "#f07ab5";
-    else if (name === "Modou") return "#20B2AA";
+    else if (name === "Modou" || name === "Modu") return "#20B2AA";
     else if (name === "Lorenzo") return "#20B2AA";
     else if (name === "Cesare") return "#f2c40d";
     else if (name === "Raluca") return "#006e0f";
